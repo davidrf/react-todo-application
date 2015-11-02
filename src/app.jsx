e52f50d696575ea745edeb14bf15cca2d9965a3c
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2015-11-02 18:03:04 -0500

    Implement basic app and header

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,25 @@
 var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./Header');
+var rootUrl = 'https://shining-heat-830.firebaseio.com/'
 
-var Hello = React.createClass({
+var App = React.createClass({
+  mixins: [ ReactFire ],
+  componentWillMount: function() {
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
   render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-md-offset-2">
+        <h2 className="text-center">
+          To-Do List
+        </h2>
+        <Header />
+      </div>
+    </div>
   }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 React.render(element, document.querySelector('.container'));
